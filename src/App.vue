3fb4bdf7d24eb7bc3a06f4389fbabd01f9a26be7
Rian-2INFO3 <rianmateussc@gmail.com>
2022-05-16 11:11:54 -0300

    feito site

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+export default {
+  components: { MenuLateral },
+};
+</script>
+<template>
+  <MenuLateral />
+  <header id="header"><h1>SITES DE ANIMES</h1></header>
+  <main id="content">
+    <div class="textop">
+      <h1>TOP 4 ANIMES</h1>
+    </div>
+    <div class="animes-list">
+      <div class="anime-item">
+        <p>BLEACH</p>
+        <img src="download.jpeg" />
+      </div>
+
+      <div class="anime-item">
+        <p>BLACK CLOVER</p>
+        <img src="op.jpg" />
+      </div>
+
+      <div class="anime-item">
+        <p>RE:ZERO</p>
+        <img src="re.jpeg" />
+      </div>
+
+      <div class="anime-item">
+        <p>TATE NO YUUSHA</p>
+        <img src="pp.jpeg" />
+      </div>
+    </div>
+  </main>
+  <footer id="footer">Rodapé</footer>
+</template>
+<style>
+.animes-list {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.anime-item {
+  display: flex;
+  flex-direction: column;
+}
+
+.anime-item p {
+  align-self: center;
+  margin-bottom: 0;
+}
+
+.anime-item img {
+  margin-top: 0;
+}
+</style>
---
+++ src/components/MenuLateral.vue
@@ -0,0 +1,15 @@
+<script>
+export default {};
+</script>
+<template>
+  <aside id="sidenav">
+    <ul>
+      <li><H1>ANIMES TV</H1></li>
+      <li><H1>ANIMES ORION</H1></li>
+      <li><H1>ANITUBE</H1></li>
+      <li><H1>GOGO ANIME</H1></li>
+      <li><H1>SUPER ANIMES</H1></li>
+    </ul>
+  </aside>
+</template>
+<style></style>
